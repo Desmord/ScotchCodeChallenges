64b468475f77633af3a1d04cf663f26c09664ee6
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2018-06-28 17:05:17 +0200

    Gotowe wyzwanie 4

--- challenge 4/js.js
+++ challenge 4/js.js
@@ -1,1 +1,16 @@
-console.log(`Witam`);
+const eyeLid = document.querySelector(`.alien__eyeLid`),
+    eyeLidTop = eyeLid.getBoundingClientRect().y,
+    eyeLidLeft = eyeLid.getBoundingClientRect().x;
+
+const mouseMove = (e) => {
+    let kat = Math.atan2(e.clientY - eyeLidTop, e.clientX - eyeLidLeft),
+        x = Number.parseInt(35 * Math.cos(kat)),
+        y = Number.parseInt(35 * Math.sin(kat));
+
+    eyeLid.style.left = `${35+x}px`;
+    eyeLid.style.top = `${35+y}px`;
+    eyeLid.style.webkitTransform = `rotate(${kat}rad)`;
+
+}
+
+document.addEventListener(`mousemove`, mouseMove);
